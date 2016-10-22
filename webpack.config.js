de9bdd7e39c4a28caa60bd0429fe481370a2b04b
MadMed677 <madmed677@gmail.com>
2016-10-23 01:09:18 +0300

    Инициализация

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const webpack = require('webpack');
+const JSDocPlugin = require('jsdoc-webpack-plugin');
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+const config = {
+    entry: [
+        'babel-regenerator-runtime',
+        './src/js/main.js'
+    ],
+
+    output: {
+        path: path.join(__dirname, 'static/js'),
+        filename: 'app.js',
+        publicPath: '/static/js/'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /node_modules/
+            }
+        ]
+    },
+
+    devServer: {
+        proxy: [{
+            path: '/api/**',
+            target: 'http://localhost:8000'
+        }],
+        port: '1111',
+        historyApiFallback: true
+    },
+
+    plugins: [
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(NODE_ENV)
+        }),
+        new JSDocPlugin({
+            conf: './jsdoc.conf'
+        })
+    ],
+
+    devtool: NODE_ENV === 'development' ? 'eval' : null
+};
+
+if (process.env.HOT) {
+    
+}
