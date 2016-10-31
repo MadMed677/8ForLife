6976cab6054345783926acfc746b1a767fa381d1
MadMed677 <madmed677@gmail.com>
2016-10-31 22:50:55 +0300

    Удалил не использующийся на данный момент barchart

--- src/js/engine/modules/chart/index.js
+++ src/js/engine/modules/chart/index.js
@@ -1,9 +1,7 @@
-import BarChart         from './bar-chart/bar-chart.react';
 import RadarChart       from './radar-chart/radar-chart.react';
 import PolarAreaChart   from './polar-area-chart/polar-area-chart.react';
 
 export {
-    BarChart,
     RadarChart,
     PolarAreaChart
 };
