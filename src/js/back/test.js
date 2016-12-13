6891fa07900b277e8d5f5b3d7364265b31d8a6bf
Artem Anashev <anashev@yamoney.ru>
2016-12-13 14:58:28 +0300

    Добавлен путь на process.env

--- src/js/back/test.js
+++ src/js/back/test.js
@@ -1,5 +1,6 @@
 const app = require('express')();
+app.set('port', (process.env.PORT || 5000));
 app.get('/', (req, res) => {
     res.send('Welcome');
 });
-app.listen('3000');
+app.listen(app.get('port'), () => console.log(`Running on port ${app.get('port')}`));
